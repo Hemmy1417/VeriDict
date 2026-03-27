f5800c782e26516627ad46b1a3e9aad169a340cf
Emmanuel <91135963+Hemmy1417@users.noreply.github.com>
2026-03-27 20:01:59 +0100

    Import React along with useEffect in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { Navbar } from "@/components/Navbar";
 import { SubmissionsTable } from "@/components/SubmissionsTable";
 import { Leaderboard } from "@/components/Leaderboard";
